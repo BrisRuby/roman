d27e32fd02994334c0d09fe3908b4125f53cbec3
Nigel Rausch <nigelr@brisbanerails.com>
2013-06-26 18:11:49 +1000

    refactored while and inject

--- lib/roman.rb
+++ lib/roman.rb
@@ -1,38 +1,19 @@
 class Roman
 
   def convert number
-    build = ''
+    #build = ''
 
-    if number >= 50
-      build += 'L'
-      number -= 50
+    brutus = [[50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]
+
+    brutus.inject('') do |b, (value, letter)|
+      while number >= value
+        b += letter
+        number -= value
+      end
+      b
     end
 
-    while number >= 10
-      build += 'X'
-      number -= 10
-    end
-
-    if number == 9
-      build += 'IX'
-      number -= 9
-    end
-
-    if number >= 5
-      build += 'V'
-      number -= 5
-    end
-
-    if number == 4
-      build += 'IV'
-      number -= 4
-    end
-
-    build += 'I' * number
-
-    build
-
-
+    #build
   end
 
 end
--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -1,4 +1,4 @@
-require 'rspec'
+require 'minitest/autorun'
 
 require_relative '../lib/roman'
 
@@ -7,60 +7,65 @@ describe Roman do
   let(:roman) { Roman.new }
 
   it 'creates a new instance of Roman' do
-    roman.should be_a Roman
+    #roman.should be_a Roman
   end
-  context 'converts integers to roman numerals' do
+  describe 'converts integers to roman numerals' do
 
     it '1 should return I' do
-      roman.convert(1).should == 'I'
+      roman.convert(1).must_equal 'I'
     end
 
     it '2 should return II' do
-      roman.convert(2).should == 'II'
+      roman.convert(2).must_equal 'II'
     end
 
     it '3 should return III' do
-      roman.convert(3).should == 'III'
+      roman.convert(3).must_equal 'III'
     end
 
     it '4 should return IV' do
-      roman.convert(4).should == 'IV'
+      roman.convert(4).must_equal 'IV'
     end
 
     it '5 should return V' do
-      roman.convert(5).should == 'V'
+      roman.convert(5).must_equal 'V'
     end
     it '6 should return VI' do
-      roman.convert(6).should == 'VI'
+      roman.convert(6).must_equal 'VI'
     end
-    it { roman.convert(8).should == 'VIIII' }
+    it '8 should return VIII' do
+      roman.convert(8).must_equal 'VIII'
+    end
 
     it '9 should return IX' do
-      roman.convert(9).should == 'IX'
+      roman.convert(9).must_equal 'IX'
     end
 
     it '10 should return X' do
-      roman.convert(10).should == 'X'
+      roman.convert(10).must_equal 'X'
     end
 
     it '11 should return XI' do
-      roman.convert(11).should == 'XI'
+      roman.convert(11).must_equal 'XI'
     end
 
     it '20 should return XX' do
-      roman.convert(20).should == 'XX'
+      roman.convert(20).must_equal 'XX'
     end
     it '42 should return XLII' do
-      roman.convert(42).should == 'XLII'
+      roman.convert(42).must_equal 'XLII'
     end
     it '50 should return L' do
-      roman.convert(50).should == 'L'
+      roman.convert(50).must_equal 'L'
+    end
+    it '89 should return LXXXIX' do
+      roman.convert(89).must_equal 'LXXXIX'
     end
 
 
 
     #it '42 should return XLII' do
-    #  roman.convert(42).should == 'XLII'
+    #  roman.convert(42).must_equal 'XLII'
     #end
 
 
