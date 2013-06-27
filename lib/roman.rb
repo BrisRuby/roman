e479db7d29cb67018731ad537c8c0bb74731b798
Ian Connor <iconnor@projectlounge.com>
2013-06-27 14:16:43 +1000

    Had to finish it

--- lib/roman.rb
+++ lib/roman.rb
@@ -3,7 +3,7 @@ class Roman
   def convert number
     #build = ''
 
-    brutus = [[50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]
+    brutus = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]
 
     brutus.inject('') do |b, (value, letter)|
       while number >= value
